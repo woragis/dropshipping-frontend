78e695ce5d490b8dfd75fe85def324e0f2c90184
woragis <masteringthecode.woragis+github@gmail.com>
2024-04-17 11:26:40 -0300

    Use home page for testing components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import InsertProductComponent from '../../components/InsertProductComponent'
+import LoginComponent from '../../components/LoginComponent'
 import ProductComponent from '../../components/Product'
+import RegisterComponent from '../../components/RegisterComponent'
 
 export const Home: React.FC = () => {
   return (
@@ -9,6 +12,20 @@ export const Home: React.FC = () => {
         Price={59.9}
         Description={'oi'}
       />
+      <br />
+      <br />
+      <h3>Login</h3>
+      <LoginComponent />
+      <br />
+      <br />
+      <hr />
+      <h3>Register</h3>
+      <RegisterComponent />
+      <br />
+      <br />
+      <hr />
+      <h3>Create Product</h3>
+      <InsertProductComponent />
     </div>
   )
 }
