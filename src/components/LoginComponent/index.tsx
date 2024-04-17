287b456a14ddab7c63155feb2fd5e53a2fc0e6f8
woragis <masteringthecode.woragis+github@gmail.com>
2024-04-17 11:25:32 -0300

    Create login component

--- src/components/LoginComponent/index.tsx
+++ src/components/LoginComponent/index.tsx
@@ -1,25 +1,54 @@
-import { FormEvent, FormEventHandler } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 const LoginComponent = () => {
-  function Login(event: SubmitEvent) {
+  interface LoginDataInterface {
+    email: string
+    password: string
+  }
+
+  const [loginData, setLoginData] = useState<LoginDataInterface>(
+    {} as LoginDataInterface
+  )
+
+  const Login = async (event: FormEvent<HTMLFormElement>) => {
+    console.log('Trying to login')
     event.preventDefault()
     try {
       const backendUrl = 'http://localhost:5000/auth/login'
-      fetch(backendUrl, {})
+      const response = await fetch(backendUrl, {
+        method: 'POST',
+        body: JSON.stringify(loginData),
+        credentials: 'include',
+      })
+      const responseData = await response.json()
+      console.log(responseData)
     } catch (err) {
       console.error(err)
     }
   }
+
+  const handleLoginChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setLoginData({ ...loginData, [event.target.id]: event.target.value })
+    console.log(loginData)
+  }
+
   return (
-    <form onSubmit={(e) => Login(e)}>
+    <form onSubmit={Login}>
       <label htmlFor="email">Email</label>
-      <input name="email" id="email" type={'email'} placeholder="email" />
+      <input
+        name="email"
+        id="email"
+        type={'email'}
+        placeholder="email"
+        onChange={handleLoginChange}
+      />
       <label htmlFor="password">Password</label>
       <input
         name="password"
         id="password"
         type={'password'}
         placeholder="password"
+        onChange={handleLoginChange}
       />
       <button>Enviar</button>
     </form>
