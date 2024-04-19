3c5cbc5fae26c862a6a48e53f2f8060d7dea603c
woragis <masteringthecode.woragis+github@gmail.com>
2024-04-19 00:47:04 -0300

    Setup first product structure

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -31,3 +31,24 @@ export const Product = ({
     </StyledProductComponent>
   )
 }
+
+export const AdminProduct = ({
+  Title,
+  Price,
+  Description,
+}: ProductComponentProps) => {
+  return (
+    <StyledProductComponent>
+      <ProductPicture />
+      <ProductInfo>
+        <ProductTitle>{Title}</ProductTitle>
+        <ProductPrice>R$ {Price}</ProductPrice>
+        <ProductDescription>
+          <p>{Description}</p>
+        </ProductDescription>
+        <button>Edit</button>
+        <button>Delete</button>
+      </ProductInfo>
+    </StyledProductComponent>
+  )
+}
