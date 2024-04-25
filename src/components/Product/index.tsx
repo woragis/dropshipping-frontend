a98b240ab539868e67f4de15c925ecf881051914
woragis <masteringthecode.woragis+github@gmail.com>
2024-04-25 17:54:53 -0300

    Implement wishlist and cart features

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -17,6 +17,53 @@ export const StoreProduct = ({
   price,
   description,
 }: ProductComponentProps) => {
+  const [cartQuantity, setCartQuantity] = useState<number>(0)
+  const addToWishlist = async () => {
+    try {
+      const backendUri = backendUriPrefix + 'wishlist'
+      const token = localStorage.getItem('token')
+      const response = await fetch(backendUri, {
+        method: 'POST',
+        body: JSON.stringify({ _id: _id }),
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+      })
+      if (response.ok) {
+        alert('Item added to wishlist')
+      }
+    } catch (err) {
+      console.error('Bad on wishlist request')
+    }
+  }
+  const addToCart = async () => {
+    try {
+      const backendUri = backendUriPrefix + 'cart'
+      const token = localStorage.getItem('token')
+      const response = await fetch(backendUri, {
+        method: 'POST',
+        body: JSON.stringify({ _id: _id, quantity: cartQuantity }),
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+      })
+      if (response.ok) {
+        alert('Item added to cart with quantity: ' + cartQuantity)
+      }
+    } catch (err) {
+      console.error('Bad on cart request')
+    }
+  }
+  const incrementCartQuantity = () => {
+    setCartQuantity((prevState) => prevState + 1)
+  }
+  const decrementCartQuantity = () => {
+    setCartQuantity((prevState) => prevState - 1)
+  }
   return (
     <StyledProductComponent key={_id}>
       <NavLink to={'/product/' + _id}>
@@ -29,6 +76,13 @@ export const StoreProduct = ({
           </ProductDescription>
         </ProductInfo>
       </NavLink>
+      <button onClick={addToWishlist}>Add to Wishlist</button>
+      <div>
+        <button onClick={incrementCartQuantity}>Increment</button>
+        <span>{cartQuantity}</span>
+        <button onClick={decrementCartQuantity}>Decrement</button>
+      </div>
+      <button onClick={addToCart}>Add to Cart</button>
     </StyledProductComponent>
   )
 }
@@ -71,11 +125,11 @@ export const AdminProduct = ({
     console.log(productData)
     setEditMode((prevState) => !prevState)
   }
-  const updateProduct = () => {
+  const updateProduct = async () => {
     try {
       const token = localStorage.getItem('token')
       const backendUri = backendUriPrefix + 'products/edit'
-      fetch(backendUri, {
+      const response = await fetch(backendUri, {
         method: 'PUT',
         body: JSON.stringify(productData),
         headers: {
@@ -83,6 +137,11 @@ export const AdminProduct = ({
           authorization: `Bearer ${token}`,
         },
       })
+      if (response.ok) {
+        editProduct()
+        console.log('Updated product')
+        alert('Updated Product')
+      }
     } catch (err) {
       console.log(err)
     }
